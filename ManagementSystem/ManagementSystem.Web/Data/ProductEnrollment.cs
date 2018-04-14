929946d7015ac3482951e2c8d29dd4e61eb83410
Lam Le <15520405@gm.uit.edu.vn>
2018-04-14 14:38:15 +0700

    Added models for product features

---
+++ ManagementSystem/ManagementSystem.Web/Data/ProductEnrollment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManagementSystem.Web.Data
+{
+    public class ProductEnrollment
+    {
+        public Guid Id { get; set; }
+
+        [ForeignKey("Product")]
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [ForeignKey("Team")]
+        public Guid TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
---
+++ ManagementSystem/ManagementSystem.Web/Data/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Web.Data
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        #region Navigation Properties
+
+        public ICollection<ProductEnrollment> ProductEnrollments { get; set; }
+
+        public ICollection<TeamEnrollment> TeamEnrollments { get; set; }
+
+        #endregion
+    }
+}
