2bdc773026988f5d097177a5af92103046f2dc20
Lee Khai <leekhai18@gmail.com>
2018-05-19 12:44:03 +0700

    Data Model - Add-F-User

--- ManagementSystem/ManagementSystem.Web/Data/ApplicationUser.cs
+++ ManagementSystem/ManagementSystem.Web/Data/ApplicationUser.cs
@@ -10,6 +10,20 @@ namespace ManagementSystem.Web.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser<Guid>
     {
+        public enum Genders
+        {
+            Lady,
+            Gentleman            
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdentityCard { get; set; }
+        public Genders Gender { get; set; }
+        public string Address { get; set; }
+        public int YearOfBirth { get; set; }
+
+
         public Salary Salary { get; set; }
 
         [ForeignKey("JobPosition")]
---
+++ ManagementSystem/ManagementSystem.Web/Migrations/20180519054255_Add-F-ApplicationUser.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ManagementSystem.Web.Migrations
+{
+    public partial class AddFApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityCard",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "YearOfBirth",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityCard",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "YearOfBirth",
+                table: "AspNetUsers");
+        }
+    }
+}
